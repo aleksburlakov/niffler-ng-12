7a23a1d6e082b845f2de64dd6ac8da25e6421bf3
aleksburlakov <aleksburlakov@yandex.ru>
2026-06-21 22:40:16 +0300

    Исправил импорт

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/data/dao/impl/UserdataUserDaoJdbc.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/data/dao/impl/UserdataUserDaoJdbc.java
@@ -3,7 +3,7 @@ package guru.qa.niffler.data.dao.impl;
 import guru.qa.niffler.config.Config;
 import guru.qa.niffler.data.Databases;
 import guru.qa.niffler.data.dao.UserdataUserDao;
-import guru.qa.niffler.data.entity.spend.UserEntity;
+import guru.qa.niffler.data.entity.UserEntity;
 import guru.qa.niffler.model.CurrencyValues;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
